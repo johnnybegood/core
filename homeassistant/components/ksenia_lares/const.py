6677f792536658a61041d0a4daa7f5011b5ab8e7
Wouter Hemeryck <wouter.hemeryck@delawareconsulting.com>
2021-01-03 09:02:26 +0000

    Lares: Use data update coordinator for zones

--- homeassistant/components/ksenia_lares/const.py
+++ homeassistant/components/ksenia_lares/const.py
@@ -1,7 +1,7 @@
 """Constants for the Ksenia Lares Alarm integration."""
 
 DOMAIN = "ksenia_lares"
-EVENT_DATA_RECEIVED = "reolink_dev-event"
+DEFAULT_TIMEOUT = 10
 
 ZONE_STATUS_ALARM = "ALARM"
 ZONE_STATUS_NORMAL = "NORMAL"
