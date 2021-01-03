8845ea28be525746f5de20de6a32587d2f35d900
Wouter Hemeryck <wouter.hemeryck@delawareconsulting.com>
2021-01-03 09:02:26 +0000

    Lares: Use data update coordinator for zones

--- homeassistant/components/ksenia_lares/binary_sensor.py
+++ homeassistant/components/ksenia_lares/binary_sensor.py
@@ -2,11 +2,20 @@
 import asyncio
 import datetime
 import logging
+import async_timeout
+
+from datetime import timedelta
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+
 from .const import (
-    EVENT_DATA_RECEIVED,
+    DEFAULT_TIMEOUT,
     ZONE_STATUS_ALARM,
     ZONE_STATUS_NOT_USED,
     ZONE_BYPASS_ON,
@@ -14,6 +23,8 @@ from .const import (
 from .base import LaresBase
 
 _LOGGER = logging.getLogger(__name__)
+
+SCAN_INTERVAL = timedelta(seconds=10)
 
 DEFAULT_DEVICE_CLASS = "motion"
 DOOR_DEVICE_CLASS = "door"
@@ -24,30 +35,45 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
 
     client = LaresBase(config_entry.data)
     descriptions = await client.zoneDescriptions()
-    zones = await client.zones()
+
+    async def async_update_data():
+        """Perform the actual updates."""
+
+        async with async_timeout.timeout(DEFAULT_TIMEOUT):
+            return await client.zones()
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="lares_zones",
+        update_method=async_update_data,
+        update_interval=SCAN_INTERVAL,
+    )
+
+    # Fetch initial data so we have data when entities subscribe
+    await coordinator.async_refresh()
 
     async_add_devices(
-        [LaresSensor(client, descriptions[idx], zone) for idx, zone in enumerate(zones)]
+        LaresSensor(coordinator, idx, descriptions[idx])
+        for idx, zone in enumerate(coordinator.data)
     )
 
 
-class LaresSensor(BinarySensorEntity):
+class LaresSensor(CoordinatorEntity, BinarySensorEntity):
     """An implementation of a Lares door/window/motion sensor."""
 
-    def __init__(self, client, description, zone):
+    def __init__(self, coordinator, idx, description):
         """Initialize a the switch."""
-        BinarySensorEntity.__init__(self)
+        super().__init__(coordinator)
 
-        self._event_state = False
-        self._last_motion = datetime.datetime.min
-        self._client = client
+        self._coordinator = coordinator
         self._description = description
-        self._zone = zone
+        self._idx = idx
 
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"lares_zones_{self._description}"
+        return f"lares_zones_{self._idx}"
 
     @property
     def name(self):
@@ -57,15 +83,14 @@ class LaresSensor(BinarySensorEntity):
     @property
     def is_on(self):
         """Return the state of the sensor."""
-        return self._zone["status"] == ZONE_STATUS_ALARM
+        return self._coordinator.data[self._idx]["status"] == ZONE_STATUS_ALARM
 
     @property
     def available(self):
         """Return True if entity is available."""
-        return (
-            self._zone["status"] != ZONE_STATUS_NOT_USED
-            or self._zone["status"] == ZONE_BYPASS_ON
-        )
+        status = self._coordinator.data[self._idx]["status"]
+
+        return status != ZONE_STATUS_NOT_USED or status == ZONE_BYPASS_ON
 
     @property
     def device_class(self):
